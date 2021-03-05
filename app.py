c8b62da0f823edd564ae72aad60c71d41911b36a
ddiazsouto <ddiazsouto@gmail.com>
2021-03-05 18:22:32 +0000

    Quite happy with the main body so far Test Driven Development -->> Tests set up for the creation of the database, will confirm that the Tables exists

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,3 +7,12 @@ app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:645202398@34.121.192
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 db=SQLAlchemy(app)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+if __name__=='__main__':
+    app.run(debug=True)
