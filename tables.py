f2fc30b02e951313fc1df0f5722e3bd434d6bd14
ddiazsouto <ddiazsouto@gmail.com>
2021-03-06 13:43:32 +0000

    Created Table Client

--- tables.py
+++ tables.py
@@ -9,5 +9,15 @@ class Employee(db.Model):
     team = db.Column(db.String(35), nullable=True)
     department = db.Column(db.String(25), nullable=False)
 
+class Client(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    company_name = db.Column(db.String(30), nullable=False)
+    contact_name = db.Column(db.String(30), nullable=False)
+    contact_surname = db.Column(db.String(30), nullable=True)
+    phone = db.Column(db.String(20), nullable=False)
+    details = db.Column(db.String(150), nullable=True)
+
+
 db.drop_all()
 db.create_all()
