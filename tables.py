a3baf94343a7474e9530437827bf0efbc9ce0d8d
ddiazsouto <ddiazsouto@gmail.com>
2021-03-08 17:03:26 +0000

    The Table Sales was created, it works well, foreign keys are defined. However still missing to be assigned relationships... see commented

--- tables.py
+++ tables.py
@@ -17,6 +17,17 @@ class Client(db.Model):
     contact_surname = db.Column(db.String(30), nullable=True)
     phone = db.Column(db.String(20), nullable=False)
     details = db.Column(db.String(150), nullable=True)
+    #customer= db.relationship('sales', backref='client')
+
+class Sales(db.Model):
+
+    #id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, primary_key=True)
+    client_id = db.Column(db.Integer, db.ForeignKey('client.id'), nullable=False)
+    employee_id = db.Column(db.Integer, db.ForeignKey('employee.id'), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    #customer= db.relationship('employee', backref='sales')
+
 
 
 db.drop_all()
