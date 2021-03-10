6d73681a8160b948e842798cd79b087cf79ba8e6
ddiazsouto <ddiazsouto@gmail.com>
2021-03-10 21:05:25 +0000

    created all the .html related with formularies at the moment, precisely: accordion.html, nemployee.html, sales.html and expenses.html

--- formularium.py
+++ formularium.py
@@ -13,6 +13,31 @@ class verifyU(FlaskForm):
     passwd= StringField('Password: ')
     submit= SubmitField('Log in')
 
-class new_client:
+class new_client(FlaskForm):
 
-    name = StringField('')
+    comp_name = StringField('Company name ')
+    contact_name= StringField('Name of the person in contact ')
+    contact_surname = StringField('Surname of the person in contact ')
+    phone = StringField('Phone number ')
+    details = StringField('Introduce client details ')
+    add_client = SubmitField('Add Client ')
+
+class new_employee(FlaskForm):
+
+    emp_name = StringField('New employee name ')
+    emp_surname= StringField('New employee Surname ')
+    role = StringField('Role ')
+    team = StringField('Team ')
+    department= StringField('Department ')
+    add_employee = SubmitField('Add employee')
+
+class new_expense(FlaskForm):
+
+    nature = StringField('Nature of the expense ')
+    vendor = StringField('Name of the vendor ')
+    reason = StringField('Reason of the expense')
+    add_expense = SubmitField('Add expense')
+
+
+
+
