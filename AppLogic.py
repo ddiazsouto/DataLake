6705c1906aa9c9fa39a42da6fd151bc1567f3cc2
Dan <destruktor@prod.us-central1-a.c.peppy-castle-305110.internal>
2021-03-20 22:36:45 +0000

    Increased tested coverage

--- AppLogic.py
+++ AppLogic.py
@@ -9,77 +9,6 @@
 
 from elementae import DanSQL
 
-# class student:
-    
-#     def __init__(self, name, age, class_):
-        
-#         self.name=name
-#         self.age=age
-#         self.class_=class_
-        
-        
-#     def avg3(self, score1, score2, score3):
-#         calc=(score1+score2+score3)/3
-#         print(calc)
-        
-    
-# getattr()
-# hasattr()
-# setattr()
-        
-        
-        
-# class stud:
-    
-#     def __init__(self):
-        
-#         self.name=''
-#         self.age=''
-#         self.class_=[]
-#         self.score=[]
-#         self.map={}
-        
-#     def avg(self):
-        
-#         j=int(input('how many exams do you want to add? '))
-#         for i in range(j):
-#             subj=input(f'What Subject was the exam number {i+1} about? ')
-#             a=input(f'{subj} exam score: ')
-#             self.score.append(int(a))
-#             self.class_.append(subj)
-        
-#         print('\n\nThe mean of all scores is: ', end='')
-#         print(sum(self.score)/len(self.score))
-        
-#     def describe(self):
-        
-#         print(f'Name: {self.name} \nSubjets: {set(self.class_)} \nScores so far: {self.score}')
-        
-    
-#     def compress(self):
-        
-               
-#         for i, j in zip(self.class_, self.score):
-            
-#             q1=i
-#             q2=j
-#             answer=[q1, q2]
-#             if q1 in self.map:
-#                 self.map[q1].append(q2)
-#             elif q1 not in self.map:
-#                 self.map[q1]=[q2]
-#             else:
-#                 print('WTF is this dude?')
-                
-#             self.class_=[]
-#             self.score=[]
-        
-
-# class alumn(stud):
-#     def __init__(self, name):
-#         super(alumn, self).__init__()
-        
-#         self.name=name
 
 
 
@@ -107,6 +36,9 @@ def process_client(grab_data):
         return grab_data, msg
 
 
+
+
+
 def navigating_client(editing):
 
         msg=''
@@ -122,6 +54,10 @@ def navigating_client(editing):
         return editing, msg
 
 
+
+
+
+
 def identitydirect(user):
 
 
@@ -133,12 +69,16 @@ def identitydirect(user):
     elif 'HR' in user.department():
         takeme_to = 'dpt-hr.html'
         
-
     elif 'Master' in user.department():
         takeme_to = 'master.html'
-       
 
+    else:
+        takeme_to = 'login.html'
+    
     return str(takeme_to)
+
+
+
 
 
 def employee_logic(grab_data, user):
--- test_sw.py
+++ test_sw.py
@@ -1,4 +1,4 @@
-from elementae import unscape, Usuarium, DanSQL, usuarios
+from elementae import unscape, Usuarium, usuarios
 import pytest
 
 
@@ -21,12 +21,38 @@ class Testuser():
 
 
 
+
+
+
+def test0():                            # Checks the efficiency of class Usuarium logging the testing
+    assert Testuser.runcheck('Test') == True
+
+def test1():                            # Checks the efficiency of class Usuarium in taking user Daniel
+    assert Testuser.runcheck('Daniel') == True
+
+def test2():                            # Checks the efficiency of class Usuarium in taking user Wesley
+    assert Testuser.runcheck('Wesley') == True
+
+def test3():                            # Checks the efficiency of class Usuarium in taking user Wesley
+    assert Testuser.retain() == True
+
+
+
+"""
+Here we test our Database interactive Object
+"""
+
+from elementae import DanSQL
+
+
+
 class MyAlchemy():
 
     def connects():
 
         try:
             attempt = DanSQL()
+            attempt.off()
             return True
         
         except: return False
@@ -43,6 +69,8 @@ class MyAlchemy():
         var = attempt.get('SELECT * FROM testbase.Test;')
         attempt.write('DROP DATABASE testbase;')
 
+        attempt.off()
+
         return str(var)    
 
 
@@ -57,19 +85,7 @@ class MyAlchemy():
 
 
 
-def test0():                            # Checks the efficiency of class Usuarium logging the testing
-    assert Testuser.runcheck('Test') == True
-
-def test1():                            # Checks the efficiency of class Usuarium in taking user Daniel
-    assert Testuser.runcheck('Daniel') == True
-
-def test2():                            # Checks the efficiency of class Usuarium in taking user Wesley
-    assert Testuser.runcheck('Wesley') == True
-
-def test3():                            # Checks the efficiency of class Usuarium in taking user Wesley
-    assert Testuser.retain() == True
-
-def test4():                            # Checks that the conection with the database is successful for the object
+def test4():                            # Is the conection with the database successful ?
     assert MyAlchemy.connects() == True
 
 def test5():                            # Checks that the object can interact with the database using an integer
@@ -86,5 +102,33 @@ def test8():
 
 
 
+"""
+Here we test the AppLogic module
+"""
 
-        
+from AppLogic import identitydirect
+
+
+
+def RedirectLogic(user_dpt):
+
+    attempt = Usuarium()
+    attempt.dpt = user_dpt                # we modify the department directly
+
+    return identitydirect(attempt)        # This method takes in a Usuarium object from 'elementae' module
+
+
+
+
+
+def test9():
+    assert RedirectLogic('Sales') == 'dpt-sales.html'    # Does the logic take us to the right template?
+
+def test10():
+    assert RedirectLogic('HR') == 'dpt-hr.html' 
+
+def test11():
+    assert RedirectLogic('Master') == 'master.html' 
+
+def test12():
+    assert RedirectLogic('testeator') == 'login.html' 
