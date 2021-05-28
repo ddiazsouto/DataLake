bba2ce75d45a20de6d8fd8abc85010aa9a32de36
ddiazsouto <77495574+ddiazsouto@users.noreply.github.com>
2021-05-28 15:30:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:645202398@34.121.192.21/main'   
-#app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:Buddhassister22@127.0.0.1/main'
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.config['SECRET_KEY']='dAnIel52'
 
@@ -51,8 +51,6 @@ Now some logic for the app and its routes
 user = Usuarium()                         # We create an object to control the user login
 
 MySQL=DanSQL()
-
-# now = datetime.now()
 
 
 
